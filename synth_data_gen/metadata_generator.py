3aafc7529effa4f6e34ae9236d572d236e99e177
Devin-Pi <18119447201@163.com>
2025-02-18 00:56:14 +0000

    Add a judgment for available coordinates

--- synth_data_gen/metadata_generator.py
+++ synth_data_gen/metadata_generator.py
@@ -14,10 +14,10 @@ class MetadataSynth:
 		self.max_duration = max_duration
 		self.stream_format = stream_format
 		self.total_duration = total_duration
-		
+
 		self.video_fps = 30 # 300ms
 		self.audio_fps = 10 # 100ms
-		
+
 		# Use assert to check if stream_format is valid
 		valid_formats = ["audio", "audiovisual"]
 		assert isinstance(self.stream_format, str) and self.stream_format in valid_formats, "Invalid stream_format"
@@ -63,7 +63,8 @@ class MetadataSynth:
 		available_coords = self.event_coords
 		for frame_number in range(self.stream_total_frames-self.max_frame_dur):
 			n_active = len(active_events) # get number of currently active events
-			for i in range(n_active, max_polyphony): 
+			used_coords_in_frame = set()
+			for i in range(n_active, max_polyphony):
 				if np.random.rand() < (max_polyphony-n_active)/(silence_weight+max_polyphony):
 					temp = list(enumerate(available_tacks))
 					if temp == []:
@@ -75,8 +76,18 @@ class MetadataSynth:
 					# choose a location at random from a trayectory
 					# this will have to be updated later to account for moving sources.
 					# we will get a list of discrete location points and update at each 100ms frame
-					idxc, (rir_id, azi, ele) = random.choice(list(enumerate(self.event_coords))) 
-					available_coords.pop(idxc) # ensure no overlapping sources at a frame (mainly for video overlays)
+					max_attempts = 0
+					while True:
+						idxc, (rir_id, azi, ele) = random.choice(list(enumerate(self.event_coords)))
+						if (azi, ele) not in used_coords_in_frame:
+							used_coords_in_frame.add((azi, ele))
+							break
+						max_attempts += 1
+						if max_attempts > len(self.event_coords):
+							break
+
+					# idxc, (rir_id, azi, ele) = random.choice(list(enumerate(self.event_coords)))
+					# available_coords.pop(idxc) # ensure no overlapping sources at a frame (mainly for video overlays)
 					# populate event metadata
 					new_event = {
 						'path': event_info["path"],
@@ -88,7 +99,7 @@ class MetadataSynth:
 						'rir_id': rir_id,
 						'azim': azi,
 						'elev': ele,
-						'duration': choose_duration, 
+						'duration': choose_duration,
 						'curr_frame': 0
 					}
 					active_events.append(new_event)
@@ -98,7 +109,7 @@ class MetadataSynth:
 			# increase frame count of active events
 			for event in active_events:
 				event['curr_frame'] += 1
-			
+
 		self.write_events_list_to_csv(events_history)
 		self.write_metadata_DCASE_format(events_history)
 
@@ -114,7 +125,7 @@ class MetadataSynth:
 									event["azim"],
 									event["elev"]])
 		self.metadata_file_csv.close()
-	
+
 	def write_metadata_DCASE_format(self, event_list):
 		# 'frame', 'class' 'trackID', 'azimuth', 'elevation', 'distance'
 		for iframe in range(self.stream_total_frames):
